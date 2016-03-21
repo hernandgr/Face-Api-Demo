6091dff47656964b9d4cdd05eade53211970787a
Hernan Guzman Rendon <hernandgr@gmail.com>
2016-03-21 16:40:36 -0500

    Added javascript example.

---
+++ Javascript/js/faceDetection.js
@@ -0,0 +1,73 @@
+$('#btn').click(function () {
+    var file = document.getElementById('filename').files[0];
+    detectFaces(file);
+});
+
+$("#filename").change(function () {
+    showImage();
+});
+
+function detectFaces(file) {
+    var apiKey = "31f51b09c8d84813b56fb08b8839e5ff";
+    
+    // Call the API
+    $.ajax({
+        url: "https://api.projectoxford.ai/face/v1.0/detect",
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+            $("#response").text("Calling api...");
+        },
+        type: "POST",
+        data: file,
+        processData: false
+    })
+        .done(function (response) {
+            // Process the API response.
+            processResult(response);
+        })
+        .fail(function (error) {
+            // Oops, an error :(
+            $("#response").text(error.getAllResponseHeaders());
+        });
+}
+
+function processResult(response) {
+    var arrayLength = response.length;
+
+    if (arrayLength > 0) {
+        var canvas = document.getElementById('myCanvas');
+        var context = canvas.getContext('2d');
+
+        context.beginPath();
+        
+        // Draw face rectangles into canvas.
+        for (var i = 0; i < arrayLength; i++) {
+            var faceRectangle = response[i].faceRectangle;
+            context.rect(faceRectangle.left, faceRectangle.top, faceRectangle.width, faceRectangle.height);
+        }
+
+        context.lineWidth = 3;
+        context.strokeStyle = 'red';
+        context.stroke();
+    }
+
+    // Show the raw response.
+    var data = JSON.stringify(response);
+    $("#response").text(data);
+}
+
+function showImage() {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    var input = document.getElementById("filename");
+    var img = new Image;
+
+    img.onload = function () {
+        context.drawImage(img, 0, 0);
+    }
+
+    img.src = URL.createObjectURL(input.files[0]);
+}
